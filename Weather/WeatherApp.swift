b36ff11c204c5b02b2e06d686fc2c2060e741d1f
Sajjad Khazraei <sajjad.kh213@gmail.com>
2023-10-28 23:08:46 +0330

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sajjad Khazraei on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
